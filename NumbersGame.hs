0bcac35cff0a6f8853e2ac39043966796456a9ff
Nicolas Chan <nicolas@nicolaschan.com>
2018-04-09 23:12:26 -0700

    Add Rational support

--- NumbersGame.hs
+++ NumbersGame.hs
@@ -1,4 +1,10 @@
-import Data.List
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import Control.Exception (try, evaluate)
+import Data.List (nub, permutations)
+import Data.Ratio (numerator, denominator)
+import GHC.Exception (SomeException)
+import System.IO.Unsafe (unsafePerformIO)
 
 data Operation a = Operation {  operation :: (a -> a -> a),
                                 toString  :: String,
@@ -27,6 +33,14 @@ instance (Show a) => Show (Expression a) where
   show (Number a) = show a
   show (Expression op e1 e2) = "(" ++ show e1 ++ show op ++ show e2 ++ ")"
 
+newtype PrettyRational = PrettyRational Rational deriving (Eq, Enum, Ord, Num, Fractional, Read) 
+default (PrettyRational)
+
+instance Show (PrettyRational) where
+  show (PrettyRational r)
+    | denominator r == 1 = show $ numerator r
+    | otherwise = show r
+
 instance (Eq a, Num a) => Eq (Expression a) where
   (==) (Number n) (Number m) = n == m
   (==) (Expression op1 e1 e2) (Expression op2 e3 e4)
@@ -36,13 +50,12 @@ instance (Eq a, Num a) => Eq (Expression a) where
   (==) _ _ = False
 
 instance (Eq a, Num a, Ord a) => Ord (Expression a) where
-  (>) e1 e2 = (evaluate e1) > (evaluate e2)
-  (<) e1 e2 = (evaluate e1) < (evaluate e2)
-  (<=) e1 e2 = (evaluate e1) <= (evaluate e2)
+  (>) e1 e2 = (eval e1) > (eval e2)
+  (<) e1 e2 = (eval e1) < (eval e2)
+  (<=) e1 e2 = (eval e1) <= (eval e2)
 
 expressionFromString :: (Num a, Read a) => String -> Expression a
-expressionFromString str
-  | head str == '(' = Number 0
+expressionFromString str | head str == '(' = Number 0
   | otherwise = Number (read str)
 
 operations :: (Fractional a, Num a, Read a, Show a) => [Operation a]
@@ -51,12 +64,29 @@ operations = [  Operation { operation = (+), toString = "+", commutative = True 
                 Operation { operation = (*), toString = "*", commutative = True },
                 Operation { operation = (/), toString = "/", commutative = False }]
 
-standardOrder :: Expression a -> Expression a
-standardOrder e = e
+eitherToMaybe :: Either e a -> Maybe a
+eitherToMaybe (Left _) = Nothing
+eitherToMaybe (Right x) = Just x
 
-evaluate :: (Num a) => Expression a -> a
-evaluate (Number n) = n
-evaluate (Expression op e1 e2) = (operation op) (evaluate e1) (evaluate e2)
+safeEvaluate :: (a -> a -> a) -> a -> a -> IO (Either SomeException a)
+safeEvaluate op left right = try (evaluate $ op left right)
+
+-- Ironic that the "safeOperation" function uses "unsafePerformIO"
+-- Let me know if this is bad and how it should be improved.
+-- Remember, I want it to work with arbitrary (Num a => a -> a -> a) functions
+safeOperation :: (a -> a -> a) -> a -> a -> Maybe a
+safeOperation op left right = unsafePerformIO $ do
+  result <- safeEvaluate op left right
+  case result of
+    Left _ -> return Nothing
+    Right x -> return (Just x)
+
+eval :: (Num a) => Expression a -> Maybe a
+eval (Number n) = Just n
+eval (Expression op e1 e2) = do
+  left <- eval e1
+  right <- eval e2
+  safeOperation (operation op) left right
 
 allPossibleExpressions :: [a] -> [Operation a] -> [Expression a]
 allPossibleExpressions ns ops = concat [ allPossibleExpressions' perm ops | perm <- permutations (map Number ns) ]
@@ -69,8 +99,13 @@ allPossibleExpressions' (e1:et) ops =
   [ Expression op e1 e | op <- ops, e <- allPossibleExpressions' et ops ] ++
   [ Expression op e (last et) | op <- ops, e <- allPossibleExpressions' (e1:init et) ops ]
 
+matches :: (Num a, Eq a) => a -> Expression a -> Bool
+matches target expr = case (eval expr) of
+  Nothing -> False
+  Just result -> result == target
+
 solve :: (Eq a, Num a) => [a] -> [Operation a] -> a -> [Expression a]
-solve ns ops target = filter (\e -> (evaluate e) == target) (allPossibleExpressions ns ops)
+solve ns ops target = filter (matches target) (allPossibleExpressions ns ops)
 
 findDifficultProblems :: (Enum a, Eq a, Num a) => [a] -> [Operation a] -> Int -> [a]
 findDifficultProblems ns ops difficulty = [ x | x <- [0..], (length . nub $ solve ns ops x) == difficulty ]
--- README.md
+++ README.md
@@ -11,12 +11,14 @@ _Objective:_ With the following constraints, find an expression that equals the 
 Using the numbers `[6,7,8,9]` and the operations `[+,-,*,/]`, how can you make `44`?
 ```hs 
 ghci> :l NumbersGame.hs
+ghci> default (PrettyRational) -- Display numbers nicely and with perfect precision
 ghci> solve [6,7,8,9] operations 44
-[(8.0*(7.0-(9.0/6.0))),((7.0-(9.0/6.0))*8.0)]
+[(8*(7-(9/6))),((7-(9/6))*8)]
 ```
 
 By default, the program supplies the four basic arithmetic operations `[+,-,*,/]` as a list called `operations`, but it is possible to add more operations by defining your own. For example, you can define exponentiation:
 ```hs
+ghci> default (Integer, Double) -- (**) doesn't work on Rationals
 ghci> let exp = Operation { operation = (**), toString = "^", commutative = False }
 ghci> solve [2,3,4] (exp:operations) 8
 [(4.0^(3.0/2.0)),((4.0-2.0)^3.0),((4.0/2.0)^3.0)]
@@ -25,13 +27,13 @@ ghci> solve [2,3,4] (exp:operations) 8
 Sometimes you will get lots of results that are very similar because operations commute. You can filter out expressions that are similar to each other with `nub` because they are considered equal.
 ```hs
 ghci> solve [6..9] operations 68
-[(8.0*(7.0+(9.0/6.0))),(((9.0/6.0)+7.0)*8.0),((7.0+(9.0/6.0))*8.0),(8.0*((9.0/6.0)+7.0))]
+[(8*(7+(9/6))),(((9/6)+7)*8),((7+(9/6))*8),(8*((9/6)+7))]
 ghci> nub $ solve [6..9] operations 68
-[(8.0*(7.0+(9.0/6.0)))]
+[(8*(7+(9/6)))]
 ```
 
 To challenge yourself or your friends, you can search for difficult target numbers. In the example below, `findDifficultProblems` searches `[0..]` for target numbers such that the length of the list of unique results is `1`.
 ```hs
 ghci> take 5 $ findDifficultProblems [6,7,8,9] operations 1
-[7.0,8.0,44.0,51.0,68.0]
+[7,8,44,51,68]
 ```
